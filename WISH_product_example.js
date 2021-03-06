3587c6e9b30e9e468fa0f5e06a1f1d52adc17fd2
Dean Ma <mrdingma@gmail.com>
2021-03-05 16:01:25 -0800

    complete

---
+++ WISH_product_example.js
@@ -0,0 +1,52 @@
+const example = {
+  Product: {
+    description: "Example description",
+    id: "5284e18fb5baba49d5xxxxxx",
+    name: "Cute Ring",
+    number_saves: "6",
+    number_sold: "0",
+    parent_sku: "Cute Bow Ring",
+    review_status: "approved",
+    removed_by_merchant: False,
+    default_shipping_price: "10.5",
+    tags: [
+      { Tag: { id: "womensring", name: "womens ring" } },
+      { Tag: { id: "jewelry", name: "Jewelry" } },
+      { Tag: { id: "bow", name: "Bow" } },
+    ],
+    variants: [
+      {
+        Variant: {
+          color: "green",
+          enabled: "True",
+          id: "5284e192b111ba49d5xxxxxx",
+          product_id: "5284e18fb5baba49d5xxxxxx",
+          inventory: "11",
+          msrp: "113.9",
+          price: "110.9",
+          shipping: "10.5",
+          localized_price: "660.0",
+          localized_shipping: "63.0",
+          localized_currency_code: "CNY",
+          sku: "AA1",
+        },
+      },
+      {
+        Variant: {
+          color: "blue",
+          enabled: "True",
+          id: "5284e19qqqbaba49d5bbbbbb",
+          product_id: "5284e18fb5baba49d5xxxxxx",
+          inventory: "100",
+          msrp: "19.9",
+          price: "15.9",
+          shipping: "10.5",
+          localized_price: "90.0",
+          localized_shipping: "63.0",
+          localized_currency_code: "CNY",
+          sku: "ZZ1",
+        },
+      },
+    ],
+  },
+};
--- task.js
+++ task.js
@@ -1,9 +1,79 @@
-// Deliverr internal item model
-const itemModel = {
-  productId: "",
-  sku: "",
-  title: "",
-};
+/**
+ * Question 1:
+ * https://merchant.wish.com/documentation/api/v2#list-all-product
+ * This api limits the results to 250 per network call. If there are more than 250 products,
+ * then multiple network calls are necessary to retrieve everything.
+ *
+ * Potential increase in efficiency:
+ * 1) if we knew how many products we had, we could send out multiple network requests at once
+ * and process via Promise.all
+ * 2) another alternative is to use their batch download API to get a CSV of all products:
+ * https://merchant.wish.com/documentation/api/v2#product-create-download-job
+ *
+ * Question 2: see below dataParser function
+ *
+ */
+
+const axios = require("axios");
+
+const database = {};
+
+async function getProducts(offset, limit = 250) {
+  try {
+    const url = "https://merchant.wish.com/api/v2/product/multi-get";
+
+    if (limit > 250) {
+      limit = 250;
+    }
+
+    if (limit < 1) {
+      limit = 1;
+    }
+
+    const response = await axios({
+      url,
+      method: "get",
+      params: {
+        limit,
+        start: offset,
+        access_token: an_example_access_token,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getAllProducts() {
+  try {
+    let response = await getProducts(0);
+    let count = 0;
+    dataParser(response);
+
+    while (response.paging.next) {
+      count++;
+      response = await getAllProducts(count * 250);
+      dataParser(response);
+    }
+  } catch (err) {
+    console.log(error);
+  }
+}
+
+function dataParser(res) {
+  res.data.forEach((obj) => {
+    const { Product } = obj;
+    const { variants } = Product;
+
+    for (let { Variant } of variants) {
+      const { id, color, sku } = Variant;
+
+      database[id] = { productId: id, sku, title: `${Product.name}-${color}` };
+    }
+  });
+}
 
 /**
  * customer use Wish marketplace to sell
@@ -27,141 +97,3 @@ const itemModel = {
  * 7) title: short, human readable item descr
  * 8) ignore warehouse parameters
  */
-
-let x = {
-  code: 0,
-  data: [
-    {
-      Product: {
-        description: "Example description",
-        id: "5284e18fb5baba49d5xxxxxx",
-        name: "Cute Ring",
-        number_saves: "6",
-        number_sold: "0",
-        parent_sku: "Cute Bow Ring",
-        review_status: "approved",
-        removed_by_merchant: False,
-        default_shipping_price: "10.5",
-        tags: [
-          { Tag: { id: "womensring", name: "womens ring" } },
-          { Tag: { id: "jewelry", name: "Jewelry" } },
-          { Tag: { id: "bow", name: "Bow" } },
-        ],
-        variants: [
-          {
-            Variant: {
-              color: "green",
-              enabled: "True",
-              id: "5284e192b111ba49d5xxxxxx",
-              product_id: "5284e18fb5baba49d5xxxxxx",
-              inventory: "11",
-              msrp: "113.9",
-              price: "110.9",
-              shipping: "10.5",
-              localized_price: "660.0",
-              localized_shipping: "63.0",
-              localized_currency_code: "CNY",
-              sku: "AA1",
-            },
-          },
-          {
-            Variant: {
-              color: "blue",
-              enabled: "True",
-              id: "5284e19qqqbaba49d5bbbbbb",
-              product_id: "5284e18fb5baba49d5xxxxxx",
-              inventory: "100",
-              msrp: "19.9",
-              price: "15.9",
-              shipping: "10.5",
-              localized_price: "90.0",
-              localized_shipping: "63.0",
-              localized_currency_code: "CNY",
-              sku: "ZZ1",
-            },
-          },
-        ],
-      },
-    },
-    {
-      Product: {
-        description: "Example Description",
-        id: "5284efafb5bab119d1zzzzzz",
-        name: "Fairisle Scarf",
-        number_saves: "0",
-        number_sold: "0",
-        parent_sku: "Fairisle Scarf",
-        review_status: "rejected",
-        removed_by_merchant: False,
-        default_shipping_price: "10.0",
-        tags: [
-          { Tag: { id: "cottonscarf", name: "cotton scarf" } },
-          { Tag: { id: "fashionaccessorie", name: "Fashion Accessories" } },
-          { Tag: { id: "fashion", name: "Fashion" } },
-          { Tag: { id: "scarf", name: "scarf" } },
-        ],
-        variants: [
-          {
-            Variant: {
-              color: "gray",
-              enabled: "True",
-              id: "5284efb1b111ba49d1qqqqqq",
-              product_id: "5284efafb5bab119d1zzzzzz",
-              inventory: "1010",
-              msrp: "25.9",
-              price: "21.91",
-              shipping: "10.0",
-              localized_price: "120.0",
-              localized_shipping: "60.0",
-              localized_currency_code: "CNY",
-              sku: "AA4",
-            },
-          },
-          {
-            Variant: {
-              color: "red",
-              enabled: "True",
-              id: "5284efbaaababa49d1eiwqdf",
-              product_id: "5284efafb5bab119d1zzzzzz",
-              inventory: "1100",
-              msrp: "21.99",
-              price: "21.91",
-              shipping: "11.0",
-              localized_price: "120.0",
-              localized_shipping: "66.0",
-              localized_currency_code: "CNY",
-              sku: "AAB",
-            },
-          },
-        ],
-      },
-    },
-  ],
-  country_shippings: [
-    {
-      CountryShipping: {
-        price: "4.99",
-        localized_price: "30.0",
-        localized_currency_code: "CNY",
-        country_code: "CA",
-      },
-    },
-    {
-      CountryShipping: {
-        price: "3.99",
-        localized_price: "24.0",
-        localized_currency_code: "CNY",
-        country_code: "US",
-      },
-    },
-  ],
-  message: "",
-  paging: {
-    next:
-      "https://merchant.wish.com/api/v2/product/multi-get?start=22&limit=2&since=2014-10-15&access_token=an_example_access_token",
-    previous:
-      "https://merchant.wish.com/api/v2/product/multi-get?start=18&limit=2&since=2014-10-15&access_token=an_example_access_token",
-  },
-};
-
-
